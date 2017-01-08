645bb7d3bea69deb93b61137ea46c03ef4f19385
Moonandeye <atimis19@gmail.com>
2017-01-09 01:14:26 +0900

    findAll practice

--- bs4Test.py
+++ bs4Test.py
@@ -38,9 +38,15 @@ bsObj = BeautifulSoup(html.read(), "lxml")
 #print(bsObj.div)
 
 html = urlopen(target)
-bsObj = BeautifulSoup(html)
+bsObj = BeautifulSoup(html, "lxml")
+
 
 nameList = bsObj.findAll("span", {"class":"green"})
 
+"""
 for name in nameList:
     print(name.get_text())
+    
+"""
+print (bsObj.findAll({"h1", "h2", "h3", "h4", "h5", "h6"}))
+
