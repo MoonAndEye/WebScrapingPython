6579d356add0afa95ec9a9feafd9a87aaf3569db
Moonandeye <atimis19@gmail.com>
2017-01-09 01:02:18 +0900

    練習 find(), findAll()

--- bs4Test.py
+++ bs4Test.py
@@ -6,12 +6,41 @@ Created on Sun Jan  8 22:40:15 2017
 """
 
 from urllib.request import urlopen
+from urllib.error import HTTPError
 from bs4 import BeautifulSoup
 
+"""
+#最簡單的 BeautifulSoup 指令
+"""
+
+def getTitle(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return ("HTTP Error")
+        
+    try:
+        bsObj = BeautifulSoup(html.read(), "lxml")
+        title = bsObj.body.h1
+    except AttributeError as e:
+        return ("ATTribute Error")
+    return title
+
+#target = "http://www.pythonscraping.com/pages/page1.html"
+target = "http://www.pythonscraping.com/pages/warandpeace.html"
+"""
 html = urlopen("http://www.pythonscraping.com/pages/page1.html")
 
-bsObj = BeautifulSoup(html.read())
+bsObj = BeautifulSoup(html.read(), "lxml")
+"""
 
-a = bsObj.h1
+#print(bsObj.h1)
+#print(bsObj.div)
 
-print(bsObj.h1)
+html = urlopen(target)
+bsObj = BeautifulSoup(html)
+
+nameList = bsObj.findAll("span", {"class":"green"})
+
+for name in nameList:
+    print(name.get_text())
